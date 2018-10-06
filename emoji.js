fc63e628a7d6097f09e784d6ce5534fda1f46da5
Claudia Magali Morales Mendez <cmagalimms@Claudias-MacBook-Pro.local>
2018-10-06 13:57:05 -0500

    New changes

--- emoji.js
+++ emoji.js
@@ -7,7 +7,8 @@
     Install the NPM module node-emoji and make sure that it's saved as a dependency.
     Save and commit these changes to the git repository
     Create a new javascript file in this directory.
-        In this file, create a function that loops through an array of emojis and logs them to the console one line at a time (minimum of 10 emoji).
+        In this file, create a function that loops through an array of emojis and logs them to the console one line at a time 
+        (minimum of 10 emoji).
         You will have to find the documentation for the node-emoji module and find how to use it.
     Save and commit any changes to the git repository
     Push these changes to GitHub
